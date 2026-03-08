e1f4261858e78cd228274dbf29b26bd7bd393cc7
voidnamespace <voidnamespace@outlook.com>
2026-03-08 20:10:40 +0400

    starting test for deactivate

--- AccountService/src/AccountService.Application/Commands/DeactivateAccountEventChain/DeactivateAccountEventChainCommand.cs
+++ AccountService/src/AccountService.Application/Commands/DeactivateAccountEventChain/DeactivateAccountEventChainCommand.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+namespace AccountService.Application.Commands.DeactivateAccountEventChain;
 
-namespace AccountService.Application.Commands.DeactivateAccountEventChain
-{
-    internal class DeactivateAccountEventChainCommand
-    {
-    }
-}
+public record DeactivateAccountEventChainCommand(Guid UserId) : IRequest;
