28041f2d763d048dad100debf8c13b5b0613b061
voidnamespace <voidnamespace@outlook.com>
2026-03-21 15:31:26 +0400

    migrations for outbox, removed rouitng key

---
+++ AuthService/src/AuthService.Infrastructure/Migrations/20260321113031_OutboxRoutingkeyRemoved.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutboxRoutingkeyRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoutingKey",
+                table: "OutboxMessages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoutingKey",
+                table: "OutboxMessages",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
