f4b6653c9ab484e09ae5e7a6de5f8c66ccacb665
voidnamespace <voidnamespace@outlook.com>
2026-04-22 03:55:59 +0400

    integrate loki centralized logging for auth service via serilog sink

--- AuthService/src/AuthService.API/Program.cs
+++ AuthService/src/AuthService.API/Program.cs
@@ -1,12 +1,22 @@
 ﻿using AuthService.API;
 using Serilog;
-
+using Serilog.Sinks.Grafana.Loki;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Warning)
     .WriteTo.Console()
     .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.GrafanaLoki(
+        "http://localhost:3100",
+        labels: new[]
+        {
+            new LokiLabel
+            {
+                Key = "service",
+                Value = "auth-service"
+            }
+        })
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
