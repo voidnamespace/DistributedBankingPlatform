1e0c8fce29dab651527aacf4d8aa15ce9761091a
voidnamespace <voidnamespace@outlook.com>
2026-05-29 01:02:59 +0400

    integration test for blacklist implemented, now e2e test for deposit

---
+++ tests/DistributedBankingPlatform.E2E.Tests/DepositFlowTests.cs
@@ -0,0 +1,10 @@
+﻿namespace DistributedBankingPlatform.E2E.Tests;
+
+public class DepositFlowTests
+{
+    [Fact]
+    public async Task Deposit_WithAuthenticatedUser_ShouldIncreaseBalance()
+    {
+
+    }
+}
